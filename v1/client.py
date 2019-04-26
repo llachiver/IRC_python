4bd173047e27ca94b74fd9312951b7752049ee15
picachoc <loic.lachiver@gmail.com>
2019-04-26 13:22:53 +0200

    client : grant, revoke, REN fix.

--- v1/client.py
+++ v1/client.py
@@ -39,8 +39,10 @@ nick = '' #user's nickname
 channel = "HUB"
 
 cmd_list={"/HELP":0,"/LIST":0,"/JOIN":1,"/LEAVE":0,
-          "/WHO":0,"/MSG":-1,"/BYE":0,"/KICK":1,"/REN":1,"/CURRENT":-1,"/NICK":1} #available commands with their number of arguments
+          "/WHO":0,"/MSG":-1,"/BYE":0,"/KICK":1,"/REN":1,"/CURRENT":-1,"/NICK":1,"/GRANT":1,"/REVOKE":1} #available commands with their number of arguments
+
 help_msg = "* /HELP: print this message\n* /LIST: list all available channels on server\n* /JOIN <channel>: join (or create) a channel\n* /LEAVE: leave current channel\n* /WHO: list users in current channel\n* <message>: send a message in current channel\n* /MSG <nick> <message>: send a private message in current channel\n* /BYE: disconnect from server\n* /KICK <nick>: kick user from current channel [admin]\n* /REN <channel>: change the current channel name [admin]"
+
 err_msg={"ERR 0": "Erreur : commande inconnue.",
          "ERR 1": "Erreur : cette commande nécessite les droits d'administrateur.",
          "ERR 2": "Erreur : vous ne pouvez pas vous cibler.",
@@ -51,7 +53,8 @@ err_msg={"ERR 0": "Erreur : commande inconnue.",
          "ERR 8": "Erreur : ce channel existe déjà.",
          "ERR 9": "Erreur : mauvais arguments. Faites /HELP.",
          "ERR 10": "Erreur : vous ne pouvez pas rejoindre le HUB (Créé en projet Réseau en 2019, le HUB est un channel particulier. Son statut particulier de \"HUB\" fait que ce channel est en fait tout simplement la zone d'attente avant de rejoindre un autre channel autre que le HUB qui est, rappelons le, un channel avant tout particulier. Par conséquent une décision particulière et nécessaire par nos soins a été adoptée, il est en conséquence particulièrement impossible d'envoyer des messages depuis le HUB. De toute manière pour savoir ce qu'il est possible de faire dans le HUB ou en dehors(zone non particulière) référez vous particulièrement au README du projet parceque si vous êtes encore là c'est que vous êtes particulièrement au chomage et que nous cassez particulièrement les sockets.)",
-         "ERR 11": "Erreur : vous ne pouvez pas JOIN votre propre channel."}
+         "ERR 11": "Erreur : vous ne pouvez pas JOIN votre propre channel.",
+         "ERR 12": "Erreur : cet utilisateur a déjà ce statut."}
 
 #--------- FUNCTIONS -------------
 
@@ -99,7 +102,7 @@ def send(msg,s):
 
     s.send(data.encode())
 
-    if(leave==1):
+    if(leave==1):           #when the user leaves a channel, we display its current location
         s.send("CURRENT".encode())
 
 
@@ -135,6 +138,7 @@ def display(data):
                 data = display_chan(words[1]) + j_nick + " a rejoint le channel."
         else:
             data = "Vous venez de créer le channel " + j_channel
+
     elif(cmd == "KICK"):
         k_adminNick, k_rank, k_nick = words[2:]
         if(k_nick==nick):
@@ -144,13 +148,16 @@ def display(data):
                 data = display_chan(words[1]) + "Vous avez kické " + display_rank(k_rank,k_nick) + "."
             else:
                 data = display_chan(words[1]) + display_rank("1",k_adminNick) + " a kické "+  display_rank(k_rank,k_nick) + "."
+
     elif(cmd == "REN"):
-        data = display_chan(words[1]) + display_rank("1",words[2]) + " a renommé le channel : " + words[3]+ " -> " + words[4]
+        data = display_chan(words[1]) + display_rank("1",words[2]) + " a renommé le channel en " + words[3]+ "."
+        
     elif(cmd == "WHO"):
         string = "Utilisateurs sur le channel :"
         for i in range(1, len(words), 2):
             string += "\n- " + display_rank(words[i],words[i+1]) 
         data = string
+        
     elif(cmd == "LEAVE"):
         if(words[3] == nick):
             data = "Vous venez de quitter le channel "+words[1]+"."
@@ -161,15 +168,34 @@ def display(data):
                     data += " Vous devenez administrateur."
                 else:
                     data += " " + words[3] +" devient administrateur."
+                    
     elif(cmd == "BYE"):
         data = words[1] + " a quitté le serveur."
+        
     elif(cmd == "CURRENT"):
         data = "Vous êtes dans le channel "+words[1]+"."
+        
     elif(cmd == "NICK"):
         rank,oldNick,newNick = words[1:]
         if(oldNick == nick):
             data = "Vous vous êtes renommé en " + newNick + "."
-        data = oldNick + " s'est renommé en " + newNick + "."
+        else:
+            data = oldNick + " s'est renommé en " + newNick + "."
+        
+    elif(cmd == "GRANT"):
+        chan, adminNick, newAdmin = words[1:]
+        if(newAdmin == nick):
+            data = display_chan(chan) + display_rank("1",adminNick) + "vous a OPé."
+        else:
+            data = display_chan(chan) + display_rank("1",adminNick) + " a OPé " + newAdmin + "."
+        
+    elif(cmd == "REVOKE"):
+        chan, adminNick, oldAdmin = words[1:]
+        if(oldAdmin == nick):
+            data = display_chan(chan) + display_rank("1",adminNick) + "vous a dé-OPé."
+        else :
+            data = display_chan(chan) + display_rank("1",adminNick) + " a dé-OPé " + oldAdmin + " admin."
+
         
     print(data)
     
